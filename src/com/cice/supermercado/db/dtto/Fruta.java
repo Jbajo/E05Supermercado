1b4b881ead7427d2fedab606012f13ab8eef9825
jbajo <javier.bajochacon@gmail.com>
2018-02-13 20:15:52 +0100

    Primer Commit

---
+++ src/com/cice/supermercado/db/dtto/Fruta.java
@@ -0,0 +1,37 @@
+package com.cice.supermercado.db.dtto;
+
+import com.cice.supermercado.business.enums.EnumCategorias;
+
+public class Fruta extends Producto{
+
+    public Fruta(EnumCategorias categoria) {
+        super();
+    }
+
+    private  String generarBaseCodigoProducto(EnumCategorias categoria){
+        String baseCodigoProducto = null;
+        baseCodigoProducto = categoria.getCodigoPais().concat("_").concat(categoria.getBaseCodigo());
+        return baseCodigoProducto;
+    }
+
+    /**
+     * Méotdo que me va a generar un código de 4 números dependiendo del nombre del producto
+     * xxxx -> 1º país (8) -> 8xxx
+     * 1019 -> 2º suma de estas cuatro cifras -> 11 -> si el numero es de 2 cifras sumamos estas -> 82xx
+     *
+     * @param baseCodigo
+     * @return codigoProducto
+     */
+
+    private  String generarCodigoProducto(String baseCodigo){
+        String codigoProducto = null;
+
+
+
+        return codigoProducto;
+
+    }
+
+
+
+}
---
+++ src/com/cice/supermercado/db/dtto/Producto.java
@@ -0,0 +1,53 @@
+package com.cice.supermercado.db.dtto;
+
+import com.cice.supermercado.business.enums.EnumCategorias;
+
+public class Producto {
+
+    private String codigo;
+    private String nombre;
+    private EnumCategorias categoria;
+    private Long stockage;
+
+    public Producto() {
+    }
+
+    public Producto(String codigo, String nombre, EnumCategorias categoria, Long stockage) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.stockage = stockage;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public EnumCategorias getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(EnumCategorias categoria) {
+        this.categoria = categoria;
+    }
+
+    public Long getStockage() {
+        return stockage;
+    }
+
+    public void setStockage(Long stockage) {
+        this.stockage = stockage;
+    }
+}
